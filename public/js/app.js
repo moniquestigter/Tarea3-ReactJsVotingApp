fbce8cdd62f19a565ebab6d9f3f0162cb4bbb04a
moniquestigter <mstigter17@hotmail.com>
2018-03-07 20:58:20 -0600

    arreglado lo de unxpected token

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ class ProductList extends React.Component {
   render() {
     return (
       <div className='ui unstackable items'>
-        Hello, friend! I am a basic React component.
+       <p> Hello, friend! I am a basic React component.</p>
       </div>
     );
   }
@@ -11,5 +11,5 @@ class ProductList extends React.Component {
 ReactDOM.render(
   <ProductList />,
   document.getElementById('content')
-);
+  );
 
