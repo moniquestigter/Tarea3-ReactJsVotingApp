c53dfd58093426e073dbf6ee2125254c918fa2b8
moniquestigter <mstigter17@hotmail.com>
2018-03-07 22:21:19 -0600

    upvote ahora sirve normal  y se utilizo el plugin transform-class-properties

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,27 @@
 
 class ProductList extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-      products: [],
-      };
-    }
+  state = {
+    products: [],
+    };
 
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
-  handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
-    }
+  handleProductUpVote = (productId) =>{ //se crea un nuevo array de productos y si se modifica, se modifica un clone envez del original
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+        }
+      });
+      this.setState({
+      products: nextProducts,
+    });
+  }
 
   render() {
     const products = this.state.products.sort((a, b) => (
@@ -41,14 +49,9 @@ class ProductList extends React.Component {
 }
 
   class Product extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleUpVote = this.handleUpVote.bind(this);
-    }
-
-    handleUpVote(){
-      this.props.onVote(this.props.id);
-    }
+    handleUpVote = () => (
+      this.props.onVote(this.props.id)
+      );
 
     render() {
         return (
